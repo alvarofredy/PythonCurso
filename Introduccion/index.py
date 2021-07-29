d980599d09afc37a370f7111c2802e2a380d84e1
Alvaro Fredy <38588276+alvarofredy@users.noreply.github.com>
2021-07-29 00:36:08 -0400

    Introduccion python

---
+++ Introduccion/index.py
@@ -0,0 +1,32 @@
+
+#comentario
+if 5 > 3:
+    print('5 es mayor a 3')
+
+if 1 > 3:
+    print('1 es mayor a 3')
+
+#####################################
+#VARIABLES
+x = 5
+y = 'hola mundo 2'
+
+print(x,y)
+
+#####################################
+#VARIABLES MULTIPLES
+a, b, c = 'uno' , 'dos' , 'tres'
+
+print(a , b , c)
+
+valor1 = valor2 = valor3 = 'mucho texto'
+
+print(valor1 , valor2 , valor3)
+
+#####################################
+#CONCATENACION
+
+nombre1 = 'Alvaro '
+nombre2 = 'Fredy'
+
+print(nombre1 + nombre2)
